1e5b6eb7e25abc546db3965bccf53f38cfc80a8c
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-03-19 09:35:35 -0400

    commit tree d6964fd8fc19a18d3cd16bf5c77381ad336dbf14

---
+++ app/helpers/order_administration_helper.rb
@@ -0,0 +1,2 @@
+module OrderAdministrationHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'admin/home', to: 'store_admin#home'
   get 'admin/orders', to: 'store_admin#orders'
   get 'admin/invoice', to: 'store_admin#invoice'
+
 end
